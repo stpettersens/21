977ace65ee880714ff7347eb239246dfeeb13017
Sam Saint-Pettersen <s.stpettersen+gh@gmail.com>
2015-06-14 00:44:45 +0000

    +Dart impl.

--- html5/dart/cards.dart
+++ html5/dart/cards.dart
@@ -20,7 +20,7 @@ class Cards {
 		this.index = -1;
 		this.deck_num = 52;
 		this.deck = new List<String>();
-		this.played = new List<int>();
+		this.played = new List<String>();
 		this.ranks = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
 		this.suits = [ "h", "d", "c", "s" ];
 	}
@@ -62,5 +62,32 @@ class Cards {
 		}
 		this.played.add(this.deck[this.index]);
 		List<String> rs = this.deck[this.index].split(" ");
+		return "[${rs[0]}${rs[1]}]";
+	}
+
+	int getValue() {
+		List<String> rs = this.deck[this.index].split(" ");
+		this.index++;
+		int value = 0;
+		if(rs[0] == "A") value = 1;
+		else if(rs[0] == "J" || rs[0] == "Q" || rs[0] == "K") value = 10;
+		else value = int.parse(rs[0]);
+		return value;
+	}
+
+	int getPlayed() {
+		return this.played.length;
+	}
+
+	List<String> drawAll() {
+		this.index = 0;
+		List<String> draws = new List<String>();
+		for(int i = 0; i < this.deck_num; i++) {
+			string d = this.draw();
+			draws.add(d);
+			this.index++;
+		}
+		this.index = -1;
+		return draws;
 	}
 }
