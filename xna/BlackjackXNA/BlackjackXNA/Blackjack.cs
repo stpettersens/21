d956f1b295dc462a019b23fa01b3c8826fd6f892
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-15 18:32:09 +0100

    ...

--- xna/BlackjackXNA/BlackjackXNA/Blackjack.cs
+++ xna/BlackjackXNA/BlackjackXNA/Blackjack.cs
@@ -1,6 +1,14 @@
+/*
+  Blackjack
+  Copyright 2015 Sam Saint-Pettersen
+  Released under the MIT/X11 License.
+
+  C#/XNA implementation
+*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -18,9 +26,62 @@ namespace BlackjackXNA
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        Texture2D blankCard;
+        Texture2D dummyCard;
+        List<string> str_hearts;
+        List<string> str_clubs;
+        List<string> str_diamonds;
+        List<string> str_spades;
+        List<Texture2D> gfx_hearts;
+        List<Texture2D> gfx_clubs;
+        List<Texture2D> gfx_diamonds;
+        List<Texture2D> gfx_spades;
+ 
         public Blackjack()
         {
+            str_hearts = new List<string>();
+            str_clubs = new List<string>();
+            str_diamonds = new List<string>();
+            str_spades = new List<string>();
+
+            for (int i = 1; i <= 13; i++)
+            {
+                if (i == 1)
+                {
+                    str_hearts.Add("hA");
+                    str_clubs.Add("cA");
+                    str_diamonds.Add("dA");
+                    str_spades.Add("sA");
+                }
+                else if (i == 11)
+                {
+                    str_hearts.Add("hJ");
+                    str_clubs.Add("cJ");
+                    str_diamonds.Add("dJ");
+                    str_spades.Add("sJ");
+                }
+                else if (i == 12)
+                {
+                    str_hearts.Add("hQ");
+                    str_clubs.Add("cQ");
+                    str_diamonds.Add("dQ");
+                    str_spades.Add("sQ");
+                }
+                else if (i == 13)
+                {
+                    str_hearts.Add("hK");
+                    str_clubs.Add("cK");
+                    str_diamonds.Add("dK");
+                    str_spades.Add("sK");
+                }
+                else
+                {
+                    str_hearts.Add("h" + i.ToString());
+                    str_clubs.Add("c" + i.ToString());
+                    str_diamonds.Add("d" + i.ToString());
+                    str_spades.Add("s" + i.ToString());
+                }
+            }
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -48,6 +109,20 @@ namespace BlackjackXNA
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            blankCard = this.Content.Load<Texture2D>("c");
+            dummyCard = this.Content.Load<Texture2D>("d");
+
+            gfx_hearts = new List<Texture2D>();
+            gfx_clubs = new List<Texture2D>();
+            gfx_diamonds = new List<Texture2D>();
+            gfx_spades = new List<Texture2D>();
+            for (int i = 0; i < str_hearts.Count; i++)
+            {
+                gfx_hearts.Add(this.Content.Load<Texture2D>(str_hearts[i]));
+                gfx_clubs.Add(this.Content.Load<Texture2D>(str_clubs[i]));
+                gfx_diamonds.Add(this.Content.Load<Texture2D>(str_diamonds[i]));
+                gfx_spades.Add(this.Content.Load<Texture2D>(str_spades[i]));
+            }
         }
 
         /// <summary>
@@ -81,10 +156,11 @@ namespace BlackjackXNA
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            GraphicsDevice.Clear(Color.Green);
+            spriteBatch.Begin();
+            Card card = new Card(gfx_hearts[0], 10, 10);
+            card.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
--- xna/BlackjackXNA/BlackjackXNA/card.cs
+++ xna/BlackjackXNA/BlackjackXNA/card.cs
@@ -5,35 +5,75 @@
 
   C#/XNA implementation
 */
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-class Card {
-  private string image;
-  private int posX;
-  private int posY;
+namespace BlackjackXNA
+{
+    class Card
+    {
+        private Texture2D image;
+        private int posX;
+        private int posY;
 
-  public Card(string card, int posX, int posY) {
-    this.image = card;
-    this.posX = posX;
-    this.posY = posY;
-  }
+        /// <summary>
+        /// Constructor for Card.
+        /// </summary>
+        /// <param name="card">Card texture</param>
+        /// <param name="posX">X position for card</param>
+        /// <param name="posY">Y position for card</param>
+        public Card(Texture2D card, int posX, int posY)
+        {
+            this.image = card;
+            this.posX = posX;
+            this.posY = posY;
+        }
 
-  public static string GetImage(string card) {
-    if(card == "c" || card == "d") {
-      return String.format("gfx/%s.png", card);
+        /// <summary>
+        /// Get card image from string designation.
+        /// E.g. Qh => hQ.png
+        /// </summary>
+        /// <param name="card">Card string designation</param>
+        /// <returns>Path to card image to draw</returns>
+        public static string GetImage(string card)
+        {
+            if (card == "c" || card == "d")
+            {
+                return String.Format("gfx/%s.png", card);
+            }
+            string suit = "";
+            return suit;
+        }
+
+        /// <summary>
+        /// Set X, Y position for card.
+        /// </summary>
+        /// <param name="posX">X position for card</param>
+        /// <param name="posY">Y position for card</param>
+        public void SetXY(int posX, int posY)
+        {
+            this.posX = posX;
+            this.posY = posY;
+        }
+
+        /// <summary>
+        /// Get X, Y position of card.
+        /// </summary>
+        /// <returns>X, Y co-ordinates of card</returns>
+        public Vector2 GetXY() {
+            return new Vector2(posX, posY);
+        }
+
+        /// <summary>
+        /// Draw the card.
+        /// </summary>
+        /// <param name="spriteBatch">Sprite batch to draw with</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(this.image, this.GetXY(), Color.White);
+        }
     }
-    string suit = "";
-  }
-
-  public void SetXY(int posX, int posY) {
-    this.posX = posX;
-    this.posY = posY;
-  }
-
-  public int[] GetXY() {
-    return [this.posX, this.posY];
-  }
-
-  public void Draw() {
-    // TODO
-  }
 }
