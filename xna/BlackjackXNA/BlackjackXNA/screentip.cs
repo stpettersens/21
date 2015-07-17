b9103b1622c3c531671d5179a80dbe6b04e8af06
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-17 13:51:45 +0100

    Update screentip.cs

--- xna/BlackjackXNA/BlackjackXNA/screentip.cs
+++ xna/BlackjackXNA/BlackjackXNA/screentip.cs
@@ -5,34 +5,40 @@
 
   C#/XNA implementation
 */
+using System;
+using System.Collections.Generic;
+using Helper;
 
-class Screentip {
-  private bool debug;
-  private int posX;
-  private int posY;
-  private string title;
-  private string msg;
-
-  public Screentip(bool debug, int posX, int posY) {
-    this.debug = debug;
-    this.posX = posX;
-    this.posY = posY;
-    Debug.Print(this.debug, String.Format("Created screentip at %d,%d", this.posX, this.posY));
-  }
-
-  public void Emit(string title, string message) {
-    this.Clear();
-    this.title = title;
-    this.msg = message;
-  }
-
-  public void Clear() {
-    this.title = "";
-    this.msg = "";
-  }
-
-  public void Draw() {
-    Debug.Print(this.debug, String.Format("Screentip is:\n%s\n%s", this.title, this.msg);
-    // TODO Actual XNA draw
+namespace BlackjackXNA
+{
+  class Screentip {
+    private bool debug;
+    private int posX;
+    private int posY;
+    private string title;
+    private string msg;
+  
+    public Screentip(bool debug, int posX, int posY) {
+      this.debug = debug;
+      this.posX = posX;
+      this.posY = posY;
+      Debugger.Emit(this.debug, String.Format("Created screentip at %d,%d", this.posX, this.posY));
+    }
+  
+    public void Emit(string title, string message) {
+      this.Clear();
+      this.title = title;
+      this.msg = message;
+    }
+  
+    public void Clear() {
+      this.title = "";
+      this.msg = "";
+    }
+  
+    public void Draw() {
+      Debugger.Emit(this.debug, String.Format("Screentip is:\n%s\n%s", this.title, this.msg);
+      // TODO Actual XNA draw
+    }
   }
 }
