036fe41833394de98028a5f3d75f5ccf28bac677
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-17 13:50:05 +0100

    Update score.cs

--- xna/BlackjackXNA/BlackjackXNA/score.cs
+++ xna/BlackjackXNA/BlackjackXNA/score.cs
@@ -5,32 +5,43 @@
 
   C#/XNA implementation
 */
+using System;
+using System.Collections.Generic;
+using Helper;
 
-class Score {
-  private bool debug;
-  private int posX;
-  private int posY;
-  private string score;
-
-  public Score(bool debug, int posX, int posY) {
-    this.debug = debug;
-    this.posX = posX;
-    this.posY = posY;
-    this.score = "";
-    Debug.Print(this.debug, String.Format("Created score counter at %d,%d", this.posX, this.posY));
-  }
-
-  public void Emit(dynamic score) {
-    this.Clear();
-    this.score = score.toString();
-  }
-
-  public void Clear() {
-    this.score = "";
-  }
-
-  public void Draw() {
-    Debug.Print(String.Format("Score is '%s'", this.score);
-    // TODO Actual XNA draw
+namespace BlackjackXNA 
+{
+  class Score 
+  {
+    private bool debug;
+    private int posX;
+    private int posY;
+    private string score;
+  
+    public Score(bool debug, int posX, int posY) 
+    {
+      this.debug = debug;
+      this.posX = posX;
+      this.posY = posY;
+      this.score = "";
+      Debugger.Emit(this.debug, String.Format("Created score counter at %d,%d", this.posX, this.posY));
+    }
+  
+    public void Emit(dynamic score) 
+    {
+      this.Clear();
+      this.score = score.toString();
+    }
+  
+    public void Clear() 
+    {
+      this.score = "";
+    }
+  
+    public void Draw(SpriteBatch spriteBatch) 
+    {
+      Debugger.Emit(String.Format("Score is '%s'", this.score);
+      // TODO Actual XNA draw
+    }
   }
 }
